75e882a230d9e6cecdcd33b1e4e1aaf9240c2fad
Diego Zurita <diegoalexandermh@gmail.com>
2023-01-09 00:32:50 -0500

    create product controller and product type controller

--- product/src/main/java/com/banquito/product/product/repository/ProductTypeRepository.java
+++ product/src/main/java/com/banquito/product/product/repository/ProductTypeRepository.java
@@ -3,9 +3,11 @@ package com.banquito.product.product.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.banquito.product.product.model.ProductType;
 
+@Repository
 public interface ProductTypeRepository extends MongoRepository<ProductType, Long>{
     List<ProductType> findAll();
     List<ProductType> findByCodeProductType(String codeProductType);    
