37fb01883a91f30ac01fb4209976be0022fa0848
Diego Zurita <diegoalexandermh@gmail.com>
2023-01-09 01:16:16 -0500

    Fix bugs

--- product/src/main/java/com/banquito/product/product/repository/ProductRepository.java
+++ product/src/main/java/com/banquito/product/product/repository/ProductRepository.java
@@ -3,11 +3,9 @@ package com.banquito.product.product.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.banquito.product.product.model.Product;
 
-@Repository
 public interface ProductRepository extends MongoRepository<Product, Long>{
     List<Product> findAll(); 
     List<Product> findByCodeProduct(String codeProduct);
