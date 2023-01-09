4b2545b169c241c61fd86843cc6c7797c745583f
Carlos Alexander Puco Logroño <44406691+carlospuco@users.noreply.github.com>
2023-01-09 00:53:19 -0500

    fixed

--- product/src/main/java/com/banquito/product/associated_service/repository/AssociatedServiceParamRepository.java
+++ product/src/main/java/com/banquito/product/associated_service/repository/AssociatedServiceParamRepository.java
@@ -8,7 +8,7 @@ import com.banquito.product.associated_service.model.AssociatedServiceParam;
 
 public interface AssociatedServiceParamRepository extends MongoRepository<AssociatedServiceParam, Long>{
     List<AssociatedServiceParam> findAll(); 
-    List<AssociatedServiceParam> findByCodeAssociatedServiceParam(String codeProduct);
+    List<AssociatedServiceParam> findByCodeAssociatedServiceParam(String codeParam);
     List<AssociatedServiceParam> findByValueType(String name);
     List<AssociatedServiceParam> findByName(String status);    
 }
