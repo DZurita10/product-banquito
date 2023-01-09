99f291f95a725b70d454911583f65b1eebd10070
pacadena5 <pacadena5@espe.edu.ec>
2023-01-09 10:19:19 -0500

    associatedServiceController complete

--- product/src/main/java/com/banquito/product/associated_service/controller/AssocietadServiceController.java
+++ product/src/main/java/com/banquito/product/associated_service/controller/AssocietadServiceController.java
@@ -1,9 +1,13 @@
 package com.banquito.product.associated_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.banquito.product.associated_service.model.AssocietadService;
 import com.banquito.product.associated_service.repository.AssocietadServiceRepository;
 
 @RestController
@@ -20,4 +24,25 @@ public class AssocietadServiceController {
     public String getTest() {
         return "/product/test a";
     }
+
+    @ResponseBody
+    @RequestMapping(value = "/associatedServices", method = RequestMethod.GET)
+    public String findAllAssociatedServices() {
+        return "TODO findAllAssociatedServices";
+    }
+
+     
+    @ResponseBody
+    @RequestMapping(value = "/associatedService", method = RequestMethod.POST)
+    public String saveAssociatedService(@RequestBody AssocietadService associetadService) {
+        return "TODO saveAssociatedService";
+    }
+
+    /* @ResponseBody
+    @RequestMapping(value = "/linkAssociatedService", method = RequestMethod.PUT)
+    public String linkProductToAssociatedService(@RequestBody Product Product) {
+        return "TODO linkProductToAssociatedService";
+    } */
+
+
 }
