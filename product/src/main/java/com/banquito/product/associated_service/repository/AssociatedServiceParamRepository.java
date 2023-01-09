7f601d7f4dd05fdb6f19e4bdf7b6922442af4031
Carlos Alexander Puco Logroño <44406691+carlospuco@users.noreply.github.com>
2023-01-09 17:56:05 -0500

    Fixed Repository

--- product/src/main/java/com/banquito/product/associated_service/repository/AssociatedServiceParamRepository.java
+++ product/src/main/java/com/banquito/product/associated_service/repository/AssociatedServiceParamRepository.java
@@ -3,11 +3,13 @@ package com.banquito.product.associated_service.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.banquito.product.associated_service.model.AssociatedServiceParam;
 
-public interface AssociatedServiceParamRepository extends MongoRepository<AssociatedServiceParam, String>{
-    List<AssociatedServiceParam> findAll(); 
-    List<AssociatedServiceParam> findByCodeAssociatedServiceParam(String codeParam);
-  
+@Repository
+public interface AssociatedServiceParamRepository extends MongoRepository<AssociatedServiceParam, String> {
+    // List<AssociatedServiceParam> findAll();
+    // List<AssociatedServiceParam> findByCodeAssociatedServiceParam(String codeParam);
+
 }
