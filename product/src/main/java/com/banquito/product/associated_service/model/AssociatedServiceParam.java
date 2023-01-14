ef5af7a2724140e452b7d00e2adb60e809ee7184
pacadena5 <pacadena5@espe.edu.ec>
2023-01-14 16:48:59 -0500

    associatedServiceParam link associatedService

--- product/src/main/java/com/banquito/product/associated_service/model/AssociatedServiceParam.java
+++ product/src/main/java/com/banquito/product/associated_service/model/AssociatedServiceParam.java
@@ -1,9 +1,6 @@
 package com.banquito.product.associated_service.model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,13 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AssociatedServiceParam {
     
-    @Id
-    public String codeParam;
-    @Indexed(unique = true)
-    
-    @Field("value_type")
     public String valueType;
-    @Field("name")
     public String name;
 
 }
