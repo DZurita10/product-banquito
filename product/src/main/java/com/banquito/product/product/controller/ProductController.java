7a7b231dd9ae3f89bc4be00d81f09890d4d101de
pacadena5 <pacadena5@espe.edu.ec>
2023-01-15 16:34:06 -0500

    rest link products to associatedServices

--- product/src/main/java/com/banquito/product/product/controller/ProductController.java
+++ product/src/main/java/com/banquito/product/product/controller/ProductController.java
@@ -1,10 +1,17 @@
 package com.banquito.product.product.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+import com.banquito.product.product.model.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
 @Controller
 @RequestMapping("api/products")
 public class ProductController {
@@ -43,5 +50,18 @@ public class ProductController {
     @RequestMapping(value = "/product", method = RequestMethod.PUT)
     public String updateProduct() {
         return "Hello Product";
+    }
+
+    //vincular una lista de productos a una lista de servicios
+    //json {"products": [{ArrayList<Products>}],
+    //      "associatedServices": [{ArrayList<Products>}]
+    //      }
+    @ResponseBody
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public String updateProduct(@RequestBody Map<String, Object> json) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Product products = objectMapper.convertValue(json.get("products"), Product.class); 
+        //Product products = ((Product)json.get("products"));
+        return products.toString();
     }
 }
